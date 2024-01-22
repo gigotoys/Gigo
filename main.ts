a144ff10c446881af1e83fa4e50017fab0a0b741
gigotoys <40448462+gigotoys@users.noreply.github.com>
2024-01-22 17:28:07 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,169 +1,103 @@
-// 在這裡添加你的程式
-//% weight=0 color=#3CB371 icon="\uf2db" block="GigoBlockly" groups='["Motor", "超音波", "顏色感測", "Receive"]'
-enum PingUnit {
-    //% block="μs"
-    MicroSeconds,
-    //% block="cm"
-    Centimeters,
-    //% block="inches"
-    Inches
-}
-enum MotorChannel {
-    //% block="A"
-    MotorA = 1,
-    //% block="B"
-    MotorB = 2,
-    //% block="C"
-    MotorC = 3,
-    //% block="D"
-    MotorD = 4
-}
-namespace gigoblockly {
-    /**馬達通道定義註解
-   A(0,14)
-   B(1,2)
-   C(8,13)
-   D(15,16)
-   */
+/**
+ * NFC reader
+ */
+//% weight=10 color=#1d8045 icon="\uf0e7" block="NFC"
+namespace NFC {
+    let myNFCevent: Action = null;
+    let receivedLen = 0;
+    let password = pins.createBuffer(6);
+    let receivedBuffer = pins.createBuffer(25);
+    let uid = pins.createBuffer(4);
+    let myRxPin = SerialPin.P2;  // 将 RX 设置为 P2
+    let myTxPin = SerialPin.P1;  // 将 TX 设置为 P1
+    let init = false;
+    password[0] = 0xFF;
+    password[1] = 0xFF;
+    password[2] = 0xFF;
+    password[3] = 0xFF;
+    password[4] = 0xFF;
+    password[5] = 0xFF;
 
+    //% advanced=true shim=NFC::RxBufferedSize
+    function RxBufferedSize(): number {
+        return 1
+    }
 
-    //% blockId=DDMmotor2 block="Motor Channel %MotorPin|speed of MSpeed(0~255) %MSpeedValue|direction of Mcontrol(0~1) %McontrolValue" blockExternalInputs=false
-    //% McontrolValue.min=0 McontrolValue.max=1 
-    //% MSpeedValue.min=0 MSpeedValue.max=255   
-    //% group="Motor"
-    export function DDMmotor2(MotorPin: MotorChannel, MSpeedValue: number, McontrolValue: number): void {
-
-        switch (MotorPin) {
-            case 1:
-                pins.analogWritePin(AnalogPin.P0, pins.map(MSpeedValue, 0, 255, 0, 1023));
-                pins.digitalWritePin(DigitalPin.P14, pins.map(McontrolValue, 0, 1, 0, 1));
-                break;
-            case 2:
-                pins.analogWritePin(AnalogPin.P1, pins.map(MSpeedValue, 0, 255, 0, 1023));
-                pins.digitalWritePin(DigitalPin.P2, pins.map(McontrolValue, 0, 1, 0, 1));
-            case 3:
-                pins.analogWritePin(AnalogPin.P8, pins.map(MSpeedValue, 0, 255, 0, 1023));
-                pins.digitalWritePin(DigitalPin.P13, pins.map(McontrolValue, 0, 1, 0, 1));
-                break;
-            case 4:
-                pins.analogWritePin(AnalogPin.P15, pins.map(MSpeedValue, 0, 255, 0, 1023));
-                pins.digitalWritePin(DigitalPin.P16, pins.map(McontrolValue, 0, 1, 0, 1));
-                break;
-
+    function wakeup(): void {
+        let myBuffer: number[] = [];
+        myBuffer = [0x55, 0x55, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x03, 0xfd, 0xd4,
+            0x14, 0x01, 0x17, 0x00];
+        let wake = pins.createBufferFromArray(myBuffer);
+        serial.writeBuffer(wake);
+        basic.pause(50);
+        receivedLen = RxBufferedSize();
+        if (receivedLen == 15) {
+            receivedBuffer = serial.readBuffer(15);
         }
     }
-    /**超音波註解
-     * Send a ping and get the echo time (in microseconds) as a result
-     * @param trig tigger pin
-     * @param echo echo pin
-     * @param unit desired conversion unit
-     * @param maxCmDistance maximum distance in centimeters (default is 500)
-     */
 
-
-
-    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
-    //% group="超音波"
-    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
-        // send pulse
-        pins.setPull(trig, PinPullMode.PullNone);
-        pins.digitalWritePin(trig, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(trig, 1);
-        control.waitMicros(10);
-        pins.digitalWritePin(trig, 0);
-
-        // read pulse
-        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
-
-        switch (unit) {
-            case PingUnit.Centimeters: return Math.idiv(d, 58);
-            case PingUnit.Inches: return Math.idiv(d, 148);
-            default: return d;
-        }
-    }
-    /**馬達註解
-    */
-    //% blockId=DDMmotor block="MSpeed pin %MSpeedPin|speed of MSpeed(0~255) %MSpeedValue|Mcontrol pin %McontrolPin|direction of Mcontrol(0~1) %McontrolValue" blockExternalInputs=false
-    //% McontrolValue.min=0 McontrolValue.max=1 
-    //% MSpeedValue.min=0 MSpeedValue.max=255   
-    //% MSpeedPin.fieldEditor="gridpicker" MSpeedPin.fieldOptions.columns=4
-    //% MSpeedPin.fieldOptions.tooltips="false" MSpeedPin.fieldOptions.width="300"
-    //% McontrolPin.fieldEditor="gridpicker" McontrolPin.fieldOptions.columns=4
-    //% McontrolPin.fieldOptions.tooltips="false" McontrolPin.fieldOptions.width="300"
-    //% group="Motor"
-    export function DDMmotor(McontrolPin: DigitalPin,McontrolValue: number,MSpeedPin: AnalogPin,MSpeedValue: number): void {
-        pins.digitalWritePin(McontrolPin, pins.map(McontrolValue, 0, 1, 0, 1));
-        pins.analogWritePin(MSpeedPin, pins.map(MSpeedValue, 0, 255, 0, 1023));	   
-    }
-    //% weight=12
-    //% block="Color Sensor init"
-    //% group="顏色感測"
-    export function ColorSensorinit(): void {
-        pins.i2cWriteNumber(41, 33276, NumberFormat.UInt16BE, false)
-        pins.i2cWriteNumber(41, 32771, NumberFormat.UInt16BE, false)
-    }
     /**
+     * Setup DFRobot NFC module Tx Rx to micro:bit pins.
+     * 設定DFRobot的Tx、Rx連接腳位
+     * @param pinTX to pinTX ,eg: SerialPin.P1
+     * @param pinRX to pinRX ,eg: SerialPin.P2
     */
-    let nowReadColor = [0, 0, 0]
-    //% weight=12
-    //% block="Color Sensor read color"
-    //% group="顏色感測"
-    export function ColorSensorReadColor(): void {
-        pins.i2cWriteNumber(41, 178, NumberFormat.Int8LE, false)
-
-        pins.i2cWriteNumber(41, 179, NumberFormat.Int8LE, false)
-
-        pins.i2cWriteNumber(41, 182, NumberFormat.Int8LE, true)
-        let TCS_RED = pins.i2cReadNumber(41, NumberFormat.UInt16BE, false)
-        pins.i2cWriteNumber(41, 184, NumberFormat.Int8LE, true)
-        let TCS_GREEN = pins.i2cReadNumber(41, NumberFormat.UInt16BE, false)
-        pins.i2cWriteNumber(41, 186, NumberFormat.Int8LE, true)
-        let TCS_BLUE = pins.i2cReadNumber(41, NumberFormat.UInt16BE, false)
-        TCS_RED = Math.round(Math.map(TCS_RED, 0, 65535, 0, 255))
-        TCS_GREEN = Math.round(Math.map(TCS_GREEN, 0, 65535, 0, 255))
-        TCS_BLUE = Math.round(Math.map(TCS_BLUE, 0, 65535, 0, 255))
-        nowReadColor = [TCS_RED, TCS_GREEN, TCS_BLUE]
-    }
-    /**
-   */
-    export enum Channel {
-        //% block="R"
-        Red = 1,
-        //% block="G"
-        Green = 2,
-        //% block="B"
-        Blue = 3
-    }
-    //% weight=12
-    //% block="Color Sensor read RGB %channel |channel"
-    //% group="顏色感測"
-    export function ColorSensorRead(channel: Channel = 1): number {
-        pins.i2cWriteNumber(41, 178, NumberFormat.Int8LE, false)
-
-        pins.i2cWriteNumber(41, 179, NumberFormat.Int8LE, false)
-
-        pins.i2cWriteNumber(41, 182, NumberFormat.Int8LE, true)
-        let TCS_RED = pins.i2cReadNumber(41, NumberFormat.UInt16BE, false)
-        pins.i2cWriteNumber(41, 184, NumberFormat.Int8LE, true)
-        let TCS_GREEN = pins.i2cReadNumber(41, NumberFormat.UInt16BE, false)
-        pins.i2cWriteNumber(41, 186, NumberFormat.Int8LE, true)
-        let TCS_BLUE = pins.i2cReadNumber(41, NumberFormat.UInt16BE, false)
-
-        let RdCl = 0
-        switch (channel) {
-            case 1:
-                RdCl = Math.round(Math.map(TCS_RED, 0, 65535, 0, 255))
-                break;
-            case 2:
-                RdCl = Math.round(Math.map(TCS_GREEN, 0, 65535, 0, 255))
-                break;
-            case 3:
-                RdCl = Math.round(Math.map(TCS_BLUE, 0, 65535, 0, 255))
-                break;
-        }
-
-        return RdCl
+    //% weight=100
+    //% blockId="NFC_setSerial" block="set NFC TX to %pinTX | RX to %pinRX at 9600 bps"
+    export function NFC_setSerial(pinTX: SerialPin, pinRX: SerialPin): void {
+        myRxPin = pinRX;
+        myTxPin = pinTX;
+        serial.redirect(
+            myRxPin,
+            myTxPin,
+            BaudRate.BaudRate9600  // 将波特率更改为 9600 bps
+        )
+        init = true;
     }
 
-}
+    //% weight=95
+    //% blockId="NFC_disconnect" block="NFC disconnect"
+    export function NFC_disconnect(): void {
+        init = false;
+    }
+
+    //% weight=94
+    //% blockId="NFC_reconnect" block="NFC reconnect"
+    export function NFC_reconnect(): void {
+        serial.redirect(
+            myRxPin,
+            myTxPin,
+            BaudRate.BaudRate9600  // 将波特率更改为 9600 bps
+        )
+        init = true;
+    }
+
+    //% weight=90
+    //% blockId="nfcEvent" block="When RFID card is detected"
+    export function nfcEvent(tempAct: Action) {
+        myNFCevent = tempAct;
+    }
+
+    //% weight=80
+    //% blockId="getUID" block="RFID UID string"
+    export function getUID(): string {
+        serial.setRxBufferSize(100)
+        wakeup();
+        let myBuffer: number[] = []
+        let uidBuffer: number[] = []
+        myBuffer = [0x00, 0x00, 0xFF, 0x04, 0xFC, 0xD4, 0x4A, 0x01, 0x00, 0xE1, 0x00]
+        let cmdUID = pins.createBufferFromArray(myBuffer)
+        serial.writeBuffer(cmdUID);
+        basic.pause(50);
+        receivedLen = RxBufferedSize();
+        if (receivedLen == 25) {
+            receivedBuffer = serial.readBuffer(25);
+            for (let i = 0; i < 4; i++) {
+                uid[i] = receivedBuffer[19 + i];
+            }
+
+            if (uid[0] == uid[1] && uid[1] == uid[2] && uid[2] == uid[3] && uid[3] == 0xFF) {
+                return "";
+            } else {
+                uidBuffer = [uid[0], uid[1], uid[2], uid[3]];
